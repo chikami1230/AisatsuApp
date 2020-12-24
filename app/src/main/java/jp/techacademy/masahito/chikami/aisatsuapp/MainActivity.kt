abbe4d47d856418e400d17681a6b20defc0470a7
chikami1230 <scell_goesit1230hs@outlook.jp>
2020-12-24 18:17:14 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/masahito/chikami/aisatsuapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package jp.techacademy.masahito.chikami.aisatsuapp
+
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity() : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button) {
+            showTimePickerDialog()
+        }
+    }
+
+    private fun showTimePickerDialog() {
+        val timePickerDialog = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { view, hour, minute ->
+                if (hour >= 2 && hour < 10) {
+                    textView.text = "おはよう"
+                } else if (hour >= 10 && hour < 18) {
+                    textView.text = "こんにちは"
+                } else{
+                    textView.text = "こんばんは"
+                }
+                Log.d("UI_PARTS", "$hour:$minute")
+            },
+            0, 0, true)
+
+        timePickerDialog.show()
+
+
+
+
+    }
+
+
+}
